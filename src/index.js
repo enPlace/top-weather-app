318516e78138ec9d1f16ae52d24cae23f060534d
enPlace <n.place@protonmail.com>
2021-07-20 11:08:41 -0300

    Add temperature info for main display
    Include high, low, current temp, and feels like information

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { geocode, getWeather } from "./forecast";
-import { populateHourly } from "./main-weather";
+import { currentTemp, populateHourly } from "./main-weather";
 
 const searchForm = document.getElementById("search-form");
 const searchBar = document.getElementById("search-bar");
@@ -8,11 +8,21 @@ const searchBar = document.getElementById("search-bar");
 searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const geocodeData = await geocode(searchBar.value);
-  console.log(geocodeData);
-  const latitude = Math.floor(geocodeData.latitude * 100) / 100;
-  const longitude = Math.floor(geocodeData.longitude * 100) / 100;
-  console.log(latitude, longitude)
-  const weatherData = await getWeather(latitude, longitude)
+  const weatherData = await getWeather(
+    Math.floor(geocodeData.latitude * 100) / 100,
+    Math.floor(geocodeData.longitude * 100) / 100
+  );
   console.log(weatherData)
-  populateHourly(weatherData)
+  populateHourly(weatherData);
+  const mainWeather = document.getElementById("main-weather")
+  mainWeather.appendChild(currentTemp(weatherData))
 });
+
+function removeChildren(parent){
+  //removes all children of an element
+  while(parent.firstChild){
+      parent.removeChild(parent.firstChild)
+  };
+}
+
+export { removeChildren }
--- src/main-weather.js
+++ src/main-weather.js
@@ -1,55 +1,84 @@
-function unixTime(unixString){
-    let dateObj = new Date(unixString*1000)
-    return dateObj.toTimeString().slice(0,5)
+import { removeChildren } from ".";
+
+function unixTime(unixString) {
+  let dateObj = new Date(unixString * 1000);
+  return dateObj.toTimeString().slice(0, 5);
 }
 
-function newHourInfo(hourlyObj){
-    const hourInfo = document.createElement("div")
-    hourInfo.classList.add("hour-info")
+function newHourInfo(hourlyObj) {
+  const hourInfo = document.createElement("div");
+  hourInfo.classList.add("hour-info");
 
-    const temp = document.createElement("div")
-    temp.classList.add("hour-temp")
-    temp.textContent = `${hourlyObj.temp}°`
-    hourInfo.appendChild(temp)
+  const temp = document.createElement("div");
+  temp.classList.add("hour-temp");
+  temp.textContent = `${hourlyObj.temp}°`;
+  hourInfo.appendChild(temp);
 
-    const iconDiv = document.createElement("div")
-    iconDiv.classList.add("hour-icon")
-    const icon = document.createElement("img")
-    icon.classList.add("small-icon")
-    icon.src = `http://openweathermap.org/img/wn/${hourlyObj.weather[0].icon}@2x.png`
-    iconDiv.appendChild(icon)
-    hourInfo.appendChild(iconDiv)
+  const iconDiv = document.createElement("div");
+  iconDiv.classList.add("hour-icon");
+  const icon = document.createElement("img");
+  icon.classList.add("small-icon");
+  icon.src = `http://openweathermap.org/img/wn/${hourlyObj.weather[0].icon}@2x.png`;
+  iconDiv.appendChild(icon);
+  hourInfo.appendChild(iconDiv);
 
-    const precipDiv = document.createElement("div")
-    precipDiv.classList.add("hour-precip")
-    precipDiv.innerHTML = "<div>precip:</div>"
-    const pop = document.createElement("div")
-    pop.textContent= `${hourlyObj.pop}%`
-    precipDiv.appendChild(pop)
-    hourInfo.appendChild(precipDiv)
+  const precipDiv = document.createElement("div");
+  precipDiv.classList.add("hour-precip");
+  precipDiv.innerHTML = "<div>precip:</div>";
+  const pop = document.createElement("div");
+  pop.textContent = `${hourlyObj.pop}%`;
+  precipDiv.appendChild(pop);
+  hourInfo.appendChild(precipDiv);
 
-    const time = document.createElement("div")
-    time.classList.add("hour-time")
-    time.textContent = unixTime(hourlyObj.dt)
-    hourInfo.appendChild(time)
+  const time = document.createElement("div");
+  time.classList.add("hour-time");
+  time.textContent = unixTime(hourlyObj.dt);
+  hourInfo.appendChild(time);
 
-    return hourInfo
+  return hourInfo;
 }
 
-function populateHourly(weatherData){
-    const hourlyContainer = document.getElementById("hourly-forecast")
-    removeChildren(hourlyContainer)
-    for(let i = 0; i<24; i++){
-        hourlyContainer.appendChild(newHourInfo(weatherData.hourly[i]))
-    }
+function populateHourly(weatherData) {
+  const hourlyContainer = document.getElementById("hourly-forecast");
+  removeChildren(hourlyContainer);
+  for (let i = 0; i < 24; i++) {
+    hourlyContainer.appendChild(newHourInfo(weatherData.hourly[i]));
+  }
 }
 
-function removeChildren(parent){
-    //removes all children of an element
-    while(parent.firstChild){
-        parent.removeChild(parent.firstChild)
-    };
+function currentTemp(weatherData) {
+  /*    const tempDiv = document.createElement("div")
+    const higlow = document.createElement("div")
+    highlow.classList.add("main-hightlow")
+    highlow.id ="main-highlow"
+    const high = document.createElement("div")
+    const low = document.createElement("div")
+    high.classList.add("current-high")
+    high.id = "current-high"
+    high.textContent = weatherData.daily[0].temp.max.slice(0,2)
+    low.classList.add("current-low")
+    low.id = "current-low" */
+  const tempDiv = document.createElement("div");
+  tempDiv.classList.add("current-temp-data");
+  tempDiv.id = "current-temp-data";
+  tempDiv.innerHTML = `
+    <div class="main-highlow" id="main-highlow">
+      <div class="current-high" id="current-high">High: ${Math.floor(
+        weatherData.daily[0].temp.max / 1
+      )}°</div>
+      <div class="current-low" id="current-low">Low: ${Math.floor(
+        weatherData.daily[0].temp.min
+      )}°</div>
+    </div>
+    <div class="current-temp" id="current-temp">
+    ${Math.floor(weatherData.current.temp / 1)}<span class="c" id="c">°C</span>
+    </div>
+    <div class="feels-like" id="feels-like">Feels like ${Math.floor(
+      weatherData.current.feels_like / 1
+    )}°C</div>
+    `;
+
+  return tempDiv;
 }
 
-
-export { populateHourly }
+export { populateHourly, currentTemp };
